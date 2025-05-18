b2554ca450c0797333c45a8e8b325154e7e95318
diyaS-15 <122123763+diyaS-15@users.noreply.github.com>
2025-05-18 01:02:36 -0400

    vercel insights

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -2,6 +2,7 @@ import About from "./Components/About";
 import Navbar from "./Components/Navbar";
 import Contact from "./Components/Contact";
 import Projects from "./Components/Projects";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
